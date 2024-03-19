a0face539dac9ff74c3c03a5236b3ce106378556
Vadim-Strakhov <mars1597@gmail.com>
2024-03-19 16:43:40 +0300

    added reactrouter v5

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,46 +1,24 @@
-import React, { useState } from "react";
-
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import SingleComic from "../singleComic/SingleComic";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
-
-import decoration from "../../resources/img/vision.png";
+import { MainPage, ComicsPage } from "../pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const onCharSelected = (id) => {
-    setChar(id);
-  };
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        {/* // _ предохранитель для ошибок */}
-        {/* <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" /> */}
-        <AppBanner />
-        <ComicsList />
-        {/* <SingleComic /> */}
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+            <Route exact path="/comics">
+              <ComicsPage />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 };
 
